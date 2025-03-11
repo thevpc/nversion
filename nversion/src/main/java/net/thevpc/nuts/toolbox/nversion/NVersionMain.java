4b86f2eecada49e78cd2c04298ce364644f0f949
thevpc <taha.bensalah@gmail.com>
2025-03-11 16:14:08 +0100

    [FIXED] fixed exception creation when Workspace is ot initialized

--- nversion/src/main/java/net/thevpc/nuts/toolbox/nversion/NVersionMain.java
+++ nversion/src/main/java/net/thevpc/nuts/toolbox/nversion/NVersionMain.java
@@ -28,7 +28,7 @@ public class NVersionMain implements NApplication {
     }
 
     public static void main(String[] args) {
-        new NVersionMain().run(NAppRunOptions.ofExit(args));
+        new NVersionMain().main(NMainArgs.ofExit(args));
     }
 
     private Set<VersionDescriptor> detectVersions(String filePath, NSession session) throws IOException {
